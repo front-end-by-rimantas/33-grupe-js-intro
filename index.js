3828bcbec1b096152b9686857fa79a97998e3692
Rimantas <belauzas@yahoo.com>
2022-01-18 10:28:59 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// single line comment
+
+/*
+multi
+line
+comment
+*/
+
+/* multi
+    line
+    comment */
+
+console.log(48526);
+console.log(true);
+console.log(false);
+console.log('Labas rytas');
