e635487c7a21170318cf3e0f055775dc5004d620
Rimantas <belauzas@yahoo.com>
2022-01-19 11:48:52 +0200

    Switch

---
+++ js/04-switch.js
@@ -0,0 +1,47 @@
+const diena = 2;
+
+switch (diena) {
+    case 1:
+        console.log('pirmadienis');
+        break;
+
+    case 2:
+        console.log('antradienis');
+        break;
+
+    case 3:
+        console.log('treciadienis');
+        break;
+
+    case 4:
+        console.log('ketvirtadienis');
+        break;
+
+    case 5:
+        console.log('penktadienis');
+        break;
+}
+
+console.log('................');
+
+const dd = 6;
+
+switch (dd) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log('darbo diena');
+        break;
+
+    case 6:
+    case 7:
+        console.log('savaitgalis');
+        break;
+
+    default:
+        console.log('tokia diena neegzistuoja');
+}
+
+console.log('................');
