45aaee666d21d87c3360aba877f0ba5cf6d244e2
Rimantas <belauzas@yahoo.com>
2022-01-20 10:45:13 +0200

    Operators

---
+++ js/06-operators.js
@@ -0,0 +1,75 @@
+/*
+https://www.w3schools.com/js/js_operators.asp
+*/
+
+const a = 5;
+const b = 7;
+const x = 127;
+
+const c = a + b;
+console.log(`${a} + ${b} = ${c}`);
+
+const d = a - b;
+console.log(`${a} - ${b} = ${d}`);
+
+const e = a * b;
+console.log(`${a} * ${b} = ${e}`);
+
+const f = a / b;
+console.log(`${a} / ${b} = ${f}`);
+
+const g = b / a;
+console.log(`${b} / ${a} = ${g}`);
+
+const h = a ** b;
+console.log(`${a} ** ${b} = ${h}`);
+
+const i = a * a * a * a * a * a * a;
+console.log(`${a} ** ${b} = ${i}`);
+
+const j = b % a;
+console.log(`${b} % ${a} = ${j}`);
+
+const k = x % a;
+console.log(`${x} % ${a} = ${k}`);
+
+const l = x % b;
+console.log(`${x} % ${b} = ${l}`);
+
+let m = 5;
+m++;
+console.log('m++', m);
+m++;
+console.log('m++', m);
+
+let n = 5;
+n--;
+console.log('n--', n);
+n--;
+console.log('n--', n);
+
+let o = 5;
+++o;
+console.log('++o', o);
+++o;
+console.log('++o', o);
+
+let p = 5;
+--p;
+console.log('--p', p);
+--p;
+console.log('--p', p);
+
+let r = 0;
+console.log(`${r++}) irasas...`);
+console.log(`${r++}) irasas...`);
+console.log(`${r++}) irasas...`);
+console.log(`${r++}) irasas...`);
+console.log(`${r++}) irasas...`);
+
+let s = 0;
+console.log(`${++s}) irasas...`);
+console.log(`${++s}) irasas...`);
+console.log(`${++s}) irasas...`);
+console.log(`${++s}) irasas...`);
+console.log(`${++s}) irasas...`);
